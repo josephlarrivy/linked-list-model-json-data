acbcb32456a08e4ce77c988fcea8dcfdb4930de0
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-16 16:17:24 -0600

    moving list over to project

--- linked-list-simplified.js
+++ linked-list-simplified.js
@@ -1,142 +1,73 @@
 class Item {
-    constructor(event) {
-        this.event = event;
-        this.prev = null;
-        this.next = null;
-    }
+  constructor(event) {
+    this.event = event;
+    this.prev = null;
+    this.next = null;
+  }
 }
-
 
 class LinkedList {
-    constructor(event) {
-        this.head = null;
-        this.tail = null;
-        this.prev = null;
-        this.length = 0;
+  constructor(event) {
+    this.head = null;
+    this.tail = null;
+    this.prev = null;
+    this.length = 0;
+    this.append(event);
+  }
 
-        this.append(event);
+  // push(event): add new value to end of list.
+  append(event) {
+    let newItem = new Item(event);
+
+    if (!this.head) {
+      this.head = newItem;
+      this.tail = this.head;
+      this.prev = null;
+    } else {
+      this.tail.next = newItem;
+      newItem.prev = this.tail
+      this.tail = newItem;
     }
+    this.length += 1;
+  }
 
-    /** push(event): add new value to end of list. */
-    append(event) {
-        let newItem = new Item(event);
-
-        if (!this.head) {
-            this.head = newItem;
-            this.tail = this.head;
-            this.prev = null;
-        } else {
-            this.tail.next = newItem;
-            newItem.prev = this.tail
-            this.tail = newItem;
-        }
-        this.length += 1;
+  // console.log() all items
+  print() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current);
+      current = current.next;
     }
+  }
 
-    print() {
-        let current = this.head;
-
-        while (current !== null) {
-            console.log(current);
-            current = current.next;
-        }
+  // adds new item to front of list
+  unshift(event) {
+    let newItem = new Item(event);
+    if (this.head === null) {
+      this.head = newItem;
+    } else {
+      this.head.prev = newItem;
+      newItem.next = this.head;
+      this.head = newItem;
+      this.head.prev = null;
     }
+    if (this.length === 0) this.tail = this.head;
+    this.length += 1;
+  }
+  
 
-    // find(val) {
-    //     let current = this.head;
 
-    //     while (current !== null) {
-    //         if (current.val === val) return true;
 
-    //         current = current.next;
-    //     }
-
-    //     return false;
-    // }
-    
 }
 
-// let list = new LinkedList({
-//     "indicent_number": "1",
-//     // "initialization_time": "date/time",
-//     // "nature_code": "xxx",
-//     // "location": {
-//     //     "address": {
-//     //         "street": "xxx",
-//     //         "city": "xxx",
-//     //         "state": "xxx",
-//     //         "zip": "xxx"
-//     //     },
-//     //     "point": {
-//     //         "latitude": "000",
-//     //         "longitude": "000"
-//     //     }
-//     // },
-//     // "outcome_status": "xxx"
-// })
 
-// list.append({
-//     "indicent_number": "2",
-//     // "initialization_time": "date/time",
-//     // "nature_code": "xxx",
-//     // "location": {
-//     //     "address": {
-//     //         "street": "xxx",
-//     //         "city": "xxx",
-//     //         "state": "xxx",
-//     //         "zip": "xxx"
-//     //     },
-//     //     "point": {
-//     //         "latitude": "000",
-//     //         "longitude": "000"
-//     //     }
-//     // },
-//     // "outcome_status": "xxx"
-// })
-
-// list.append({
-//     "indicent_number": "3",
-//     // "initialization_time": "date/time",
-//     // "nature_code": "xxx",
-//     // "location": {
-//     //     "address": {
-//     //         "street": "xxx",
-//     //         "city": "xxx",
-//     //         "state": "xxx",
-//     //         "zip": "xxx"
-//     //     },
-//     //     "point": {
-//     //         "latitude": "000",
-//     //         "longitude": "000"
-//     //     }
-//     // },
-//     // "outcome_status": "xxx"
-// })
-
-// list.append({
-//     "indicent_number": "4",
-//     // "initialization_time": "date/time",
-//     // "nature_code": "xxx",
-//     // "location": {
-//     //     "address": {
-//     //         "street": "xxx",
-//     //         "city": "xxx",
-//     //         "state": "xxx",
-//     //         "zip": "xxx"
-//     //     },
-//     //     "point": {
-//     //         "latitude": "000",
-//     //         "longitude": "000"
-//     //     }
-//     // },
-//     // "outcome_status": "xxx"
-// })
 
 let list = new LinkedList(1)
 list.append(2)
 list.append(3)
 list.append(4)
 list.append(5)
+list.unshift(6)
 
 
 // console.log(list)
